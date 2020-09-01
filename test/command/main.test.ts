88cfeec347b5378695a3d7550400e0ec8707d2a1
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2020-09-01 16:30:18 -0400

    update command tests

--- test/command/main.test.ts
+++ test/command/main.test.ts
@@ -1,45 +1,45 @@
 import {expect, fancy} from 'fancy-test'
 
-import {Main} from '../src/main'
-import * as PluginHelp from '@oclif/plugin-help'
-import * as Config from '@oclif/config'
+import {Main} from '../../src/command/main'
+import * as PluginHelp from '../../src/help'
+import * as Config from '../../src/config'
 import {TestHelpClassConfig} from './helpers/test-help-in-src/src/test-help-plugin'
+import path = require('path')
 
-const pjson = require('../package.json')
-const version = `@oclif/command/${pjson.version} ${process.platform}-${process.arch} node-${process.version}`
+const pjson = require(path.resolve(__dirname, '../../package.json'))
+const version = `@oclif/core/${pjson.version} ${process.platform}-${process.arch} node-${process.version}`
 const originalgetHelpClass = PluginHelp.getHelpClass
 
 describe('main', () => {
   fancy
   .stdout()
   .do(() => Main.run(['plugins']))
-  .do(output => expect(output.stdout).to.equal('no plugins installed\n'))
+  .do((output: any) => expect(output.stdout).to.equal('no plugins installed\n'))
   .it('runs plugins')
 
   fancy
   .stdout()
   .do(() => Main.run(['-v']))
   .catch('EEXIT: 0')
-  .do(output => expect(output.stdout).to.equal(version + '\n'))
+  .do((output: any) => expect(output.stdout).to.equal(version + '\n'))
   .it('runs -v')
 
   fancy
   .stdout()
   .do(() => Main.run(['-h']))
   .catch('EEXIT: 0')
-  .do(output => expect(output.stdout).to.equal(`oclif base command
+  .do((output: any) => expect(output.stdout).to.equal(`base library for oclif CLIs
 
 VERSION
   ${version}
 
 USAGE
-  $ @oclif/command [COMMAND]
+  $ oclif [COMMAND]
 
 TOPICS
   plugins  list installed plugins
 
 COMMANDS
-  help     display help for @oclif/command
   plugins  list installed plugins
 
 `))
@@ -48,7 +48,7 @@ COMMANDS
   describe('with an alternative help class', async () => {
     const getMainWithHelpClass = async () => {
       const config: TestHelpClassConfig = await Config.load()
-      config.pjson.oclif.helpClass = './lib/test-help-plugin'
+      config.pjson.oclif.helpClass = './src/test-help-plugin'
 
       class MainWithHelpClass extends Main {
         config = config
@@ -59,47 +59,47 @@ COMMANDS
 
     fancy
     .stdout()
-    .stub(PluginHelp, 'getHelpClass', function (config: Config.IConfig) {
+    .stub(PluginHelp, 'getHelpClass', ((config: Config.IConfig) => {
       const patchedConfig = {
         ...config,
         root: `${__dirname}/helpers/test-help-in-src/`,
       }
 
       return originalgetHelpClass(patchedConfig)
-    })
+    }) as unknown as () => void)
     .do(async () => (await getMainWithHelpClass()).run(['-h']))
     .catch('EEXIT: 0')
-    .do(output => expect(output.stdout).to.equal('hello showHelp\n'))
+    .do((output: any) => expect(output.stdout).to.equal('hello showHelp\n'))
     .it('works with -h')
 
     fancy
     .stdout()
-    .stub(PluginHelp, 'getHelpClass', function (config: Config.IConfig) {
+    .stub(PluginHelp, 'getHelpClass', ((config: Config.IConfig) =>  {
       const patchedConfig = {
         ...config,
         root: `${__dirname}/helpers/test-help-in-src/`,
       }
 
       return originalgetHelpClass(patchedConfig)
-    })
+    }) as unknown as () => void)
     .do(async () => (await getMainWithHelpClass()).run(['--help']))
     .catch('EEXIT: 0')
-    .do(output => expect(output.stdout).to.equal('hello showHelp\n'))
+    .do((output: any) => expect(output.stdout).to.equal('hello showHelp\n'))
     .it('works with --help')
 
     fancy
     .stdout()
-    .stub(PluginHelp, 'getHelpClass', function (config: Config.IConfig) {
+    .stub(PluginHelp, 'getHelpClass', ((config: Config.IConfig) => {
       const patchedConfig = {
         ...config,
         root: `${__dirname}/helpers/test-help-in-src/`,
       }
 
       return originalgetHelpClass(patchedConfig)
-    })
+    }) as unknown as () => void)
     .do(async () => (await getMainWithHelpClass()).run(['help']))
     .catch('EEXIT: 0')
-    .do(output => expect(output.stdout).to.equal('hello showHelp\n'))
+    .do((output: any) => expect(output.stdout).to.equal('hello showHelp\n'))
     .it('works with help')
   })
 })
