b19b46d4e625c6e26a4e001a57b4f12edc60dd3e
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2020-09-16 13:44:03 -0500

    fix tests

--- test/config/ts-node.test.ts
+++ test/config/ts-node.test.ts
@@ -2,7 +2,7 @@ import * as path from 'path'
 import * as proxyquire from 'proxyquire'
 import * as tsNode from 'ts-node'
 
-import {TSConfig} from '../../src/config/ts-node'
+import {Interfaces} from '../../src'
 
 import {expect, fancy} from './test'
 
@@ -10,18 +10,18 @@ const root = path.resolve(__dirname, 'fixtures/typescript')
 const orig = 'src/hooks/init.ts'
 let tsNodeRegisterCallArguments: any[] = []
 
-const DEFAULT_TS_CONFIG: TSConfig = {
+const DEFAULT_TS_CONFIG: Interfaces.TSConfig = {
   compilerOptions: {},
 }
 
-const withMockTsConfig = (config: TSConfig = DEFAULT_TS_CONFIG) => {
+const withMockTsConfig = (config: Interfaces.TSConfig = DEFAULT_TS_CONFIG) => {
   const tsNodePlugin = proxyquire('../../src/config/ts-node', {fs: {
     existsSync: () => true,
     readFileSync: () => JSON.stringify(config),
   }})
 
-  // This prints "loadTSConfig unstubbed" not "loadTSConfig proxyquire"!
-  tsNodePlugin.tsPath('poop', 'asdf')
+  // This prints "loadInterfaces.TSConfig unstubbed" not "loadInterfaces.TSConfig proxyquire"!
+  tsNodePlugin.tsPath('qwerty', 'asdf')
 
   return fancy
   .add('tsNodePlugin', () => tsNodePlugin)
