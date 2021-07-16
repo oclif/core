215753402e27b3dc27ee23b7b65e98cd1719420e
peternhale <peternhale@users.noreply.github.com>
2021-07-16 09:40:08 -0600

    Update test/config/config.test.ts
    Co-authored-by: Rodrigo Espinosa de los Monteros <1084688+RodEsp@users.noreply.github.com>

--- test/config/config.test.ts
+++ test/config/config.test.ts
@@ -300,6 +300,7 @@ describe('Config', () => {
     const command = config.findCommand('foo:bar', {must: true})
     expect(command).to.have.property('id', 'foo:bar')
     expect(command).to.have.property('pluginType', 'core')
+    expect(command).to.have.property('pluginAlias', '@My/plugina')
   })
   findCommandTestConfig({commandIds: ['foo:bar', 'foo:bar'], types: ['user', 'core']})
   .it('find command with duplicates core/user', config => {
