b59fe5ec8c2e9d0be7f6c472c9e496878a051075
Mike Donnalley <mdonnalley@salesforce.com>
2022-01-06 15:38:00 -0700

    chore: fix sf integration tests

--- test/integration/sf.e2e.ts
+++ test/integration/sf.e2e.ts
@@ -4,6 +4,7 @@ import {Executor, setup} from './util'
 describe.only('Salesforce CLI (sf)', () => {
   let executor: Executor
   before(async () => {
+    process.env.SFDX_TELEMETRY_DISABLE_ACKNOWLEDGEMENT = 'true'
     executor = await setup(__filename, {repo: 'git@github.com:salesforcecli/cli.git'})
   })
 
@@ -45,7 +46,6 @@ describe.only('Salesforce CLI (sf)', () => {
 
   it('should have formatted json success output', async () => {
     const config = await executor.executeCommand('config list --json')
-    console.log(config)
     const result = JSON.parse(config.output!)
     expect(result).to.have.property('status')
     expect(result).to.have.property('result')
@@ -60,5 +60,15 @@ describe.only('Salesforce CLI (sf)', () => {
     expect(result).to.have.property('name')
     expect(result).to.have.property('message')
     expect(result).to.have.property('warnings')
+  })
+
+  it('should handle varags', async () => {
+    const config = await executor.executeCommand('config set disableTelemetry=true restDeploy=true --global --json')
+    const parsed = JSON.parse(config.output!)
+    expect(parsed.status).to.equal(0)
+    const results = parsed.result as Array<{success: boolean}>
+    for (const result of results) {
+      expect(result.success).to.be.true
+    }
   })
 })
