136ffe06fe3dc3ddb6d018ced2b2cfaa9399d943
peternhale <peternhale@users.noreply.github.com>
2022-02-11 11:36:22 -0700

    fix: use error type instead of record (#371)
    @W-10310042@

--- src/command.ts
+++ src/command.ts
@@ -224,7 +224,7 @@ export default abstract class Command {
     return Parser.parse(argv, opts)
   }
 
-  protected async catch(err: Record<string, any>): Promise<any> {
+  protected async catch(err: Error & {exitCode?: number}): Promise<any> {
     process.exitCode = process.exitCode ?? err.exitCode ?? 1
     if (this.jsonEnabled()) {
       CliUx.ux.styledJSON(this.toErrorJson(err))
