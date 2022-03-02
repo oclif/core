7c920de16081240c5f8b53525951a2e2e8096816
Mike Donnalley <mdonnalley@salesforce.com>
2022-03-02 11:54:09 -0800

    Update test/config/util.test.ts
    Co-authored-by: Rodrigo Espinosa de los Monteros <1084688+RodEsp@users.noreply.github.com>

--- test/config/util.test.ts
+++ test/config/util.test.ts
@@ -30,5 +30,28 @@ describe('util', () => {
         'baz:bar:foo',
       ])
     })
+    
+    // This is just calculating the factorial (n!) of the number of elements passed in.
+    const numberOfPermutations = (commandID: string): number => {
+      const num = commandID.split(':').length
+      let result = 1
+      for (let i = 2; i <= num; i++)
+        result *= i
+      return result
+    }
+
+    test
+    .it('returns the correct number of permutations', async () => {
+      expect(getCommandIdPermutations('one').length).to.equal(numberOfPermutations('one'))
+      expect(getCommandIdPermutations('one:two').length).to.equal(numberOfPermutations('one:two'))
+      expect(getCommandIdPermutations('one:two:three').length).to.equal(numberOfPermutations('one:two:three'))
+      expect(getCommandIdPermutations('one:two:three:four').length).to.equal(numberOfPermutations('one:two:three:four'))
+      expect(getCommandIdPermutations('one:two:three:four:five').length).to.equal(numberOfPermutations('one:two:three:four:five'))
+      expect(getCommandIdPermutations('one:two:three:four:five:six').length).to.equal(numberOfPermutations('one:two:three:four:five:six'))
+      expect(getCommandIdPermutations('one:two:three:four:five:six:seven').length).to.equal(numberOfPermutations('one:two:three:four:five:six:seven'))
+      expect(getCommandIdPermutations('one:two:three:four:five:six:seven:eight').length).to.equal(numberOfPermutations('one:two:three:four:five:six:seven:eight'))
+      expect(getCommandIdPermutations('one:two:three:four:five:six:seven:eight:nine').length).to.equal(numberOfPermutations('one:two:three:four:five:six:seven:eight:nine'))
+      expect(getCommandIdPermutations('one:two:three:four:five:six:seven:eight:nine:ten').length).to.equal(numberOfPermutations('one:two:three:four:five:six:seven:eight:nine:ten'))
+    })
   })
 })
