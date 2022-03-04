03441d7ce1c224440f7d55ffd35a1337e0c410fd
Peter Hale <peter.hale@salesforce.com>
2022-03-04 12:21:45 -0700

    chore: fix copy paste errors

--- test/cli-ux/styled/header.test.ts
+++ test/cli-ux/styled/header.test.ts
@@ -2,10 +2,10 @@ import {expect, fancy} from 'fancy-test'
 
 import {CliUx} from '../../../src'
 
-describe('styled/object', () => {
+describe('styled/header', () => {
   fancy
   .stdout()
-  .end('shows a table', output => {
+  .end('shows a styled header', output => {
     CliUx.ux.styledHeader('A styled header')
     expect(output.stdout).to.equal('=== A styled header\n\n')
   })
