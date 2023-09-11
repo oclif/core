0c607e5550a5842dc7c4517f1c42f5ae1e040d8f
Mike Donnalley <mdonnalley@salesforce.com>
2023-09-11 14:47:57 -0600

    chore: test debugging

--- test/integration/util.ts
+++ test/integration/util.ts
@@ -188,7 +188,7 @@ export async function setup(testFile: string, options: SetupOptions): Promise<Ex
   executor.debug(`${bin}_CONFIG_DIR:`, process.env[`${bin}_CONFIG_DIR`])
   executor.debug(`${bin}_CACHE_DIR:`, process.env[`${bin}_CACHE_DIR`])
 
-  const yarnInstallRes = await executor.executeInTestDir('yarn install --force')
+  const yarnInstallRes = await executor.executeInTestDir('yarn install --force', {silent: false})
   if (yarnInstallRes.code !== 0) {
     console.error(yarnInstallRes?.error)
     throw new Error('Failed to run `yarn install --force`')
