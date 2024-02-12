e54ed7628a49ad7ae479191b637e47a4abdc32c9
Mike Donnalley <mdonnalley@salesforce.com>
2024-02-12 13:22:25 -0700

    chore: formatting

--- src/execute.ts
+++ src/execute.ts
@@ -13,7 +13,7 @@ import {settings} from './settings'
  * @example For ESM dev.js
  * ```
  * #!/usr/bin/env -S node --loader ts-node/esm --no-warnings=ExperimentalWarning
- * import { execute } from "@oclif/core"
+ * import { execute } from '@oclif/core'
  *
  * await execute({development: true, dir: import.meta.url})
  * ```
@@ -21,7 +21,7 @@ import {settings} from './settings'
  * @example For ESM run.js
  * ```
  * #!/usr/bin/env node
- * import { execute } from "@oclif/core"
+ * import { execute } from '@oclif/core'
  *
  * await execute({dir: import.meta.url})
  * ```
