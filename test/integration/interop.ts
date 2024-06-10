907238772caa67440d3538a5cee047322f79780c
Mike Donnalley <mdonnalley@salesforce.com>
2024-06-10 09:35:54 -0600

    test: debug failing test

--- test/integration/interop.ts
+++ test/integration/interop.ts
@@ -261,9 +261,6 @@ async function testRunner({
       command: plugin.command,
       script: devExecutable,
       expectStrings: ['cheers', plugin.hookText],
-      env: {
-        DEBUG: '*ts-path',
-      },
     })
 
     await cleanUp({executor, plugin, script: 'run'})
