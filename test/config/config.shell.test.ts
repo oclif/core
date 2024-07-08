67d72c61d7dcc6d86d2af28a158567a8e45f7bd0
Mike Donnalley <mdonnalley@salesforce.com>
2024-07-08 13:26:21 -0600

    test: windows shell test

--- test/config/config.shell.test.ts
+++ test/config/config.shell.test.ts
@@ -5,7 +5,7 @@ import {sep} from 'node:path'
 import {Config} from '../../src'
 
 const getShell = () =>
-  osUserInfo().shell?.split(sep)?.pop() || process.platform === 'win32' ? 'powershell' : 'unknown'
+  osUserInfo().shell?.split(sep)?.pop() || (process.platform === 'win32' ? 'powershell' : 'unknown')
 
 describe('config shell', () => {
   it('has a default shell', () => {
