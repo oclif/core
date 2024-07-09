bcec7dfbcb27579ddd312f8ebda5a7b6c0829e98
Mike Donnalley <mdonnalley@salesforce.com>
2024-07-09 08:57:20 -0600

    fix: correctly identify powershell on windows (#1134)
    * fix: correctly identify powershell
    
    * test: windows shell test
    
    * test: windows shell test
    
    * test: load config
    
    ---------
    
    Co-authored-by: Cristian Dominguez <cdominguez@salesforce.com>

--- test/config/config.shell.test.ts
+++ test/config/config.shell.test.ts
@@ -4,11 +4,22 @@ import {sep} from 'node:path'
 
 import {Config} from '../../src'
 
-const getShell = () => osUserInfo().shell?.split(sep)?.pop() || 'unknown'
+const getShell = () =>
+  osUserInfo().shell?.split(sep)?.pop() || (process.platform === 'win32' ? 'powershell' : 'unknown')
 
 describe('config shell', () => {
-  it('has a default shell', () => {
-    const config = new Config({root: '/tmp'})
+  it('has a default shell', async () => {
+    const config = new Config({
+      root: '/tmp',
+      pjson: {
+        name: 'test-cli',
+        version: '0.0.1',
+        oclif: {
+          bin: 'test-cli',
+        },
+      },
+    })
+    await config.load()
     // @ts-expect-error because _shell is private
     expect(config._shell()).to.equal(getShell(), `SHELL: ${process.env.SHELL} COMSPEC: ${process.env.COMSPEC}`)
   })
