e4dd03c70d212bf0d1736358b2bca073d5e49f12
Jamie Feingold <jfeingold@salesforce.com>
2026-06-10 12:38:52 -0500

    fix: causative errors are now also pretty-printed. @W-22797799@

--- test/errors/pretty-print.test.ts
+++ test/errors/pretty-print.test.ts
@@ -7,7 +7,7 @@ import prettyPrint from '../../src/errors/errors/pretty-print'
 import {PrettyPrintableError} from '../../src/interfaces/errors'
 
 describe('pretty-print', () => {
-  it('pretty prints an error', async () => {
+  it('pretty prints a simple error', async () => {
     const sampleError: Error & PrettyPrintableError = new Error('Something very serious has gone wrong with the flags!')
     sampleError.ref = 'https://oclif.io/docs/flags'
     sampleError.code = 'OCLIF_BAD_FLAG'
@@ -18,6 +18,23 @@ describe('pretty-print', () => {
     Code: OCLIF_BAD_FLAG
     Try this: Try using using a good flag
     Reference: https://oclif.io/docs/flags`)
+  })
+
+  it('pretty prints an error and its causative error chain', async () => {
+    const rootError: Error & PrettyPrintableError = new Error('This error is the root error')
+    const errorOnceRemoved: Error & PrettyPrintableError = new Error(
+      'This error is one level removed from the root error',
+      {cause: rootError},
+    )
+    const errorTwiceRemoved: Error & PrettyPrintableError = new Error(
+      'This error is two levels removed from the root error',
+      {cause: errorOnceRemoved},
+    )
+
+    expect(ansis.strip(prettyPrint(errorTwiceRemoved) ?? '')).to
+      .equal(`    Error: This error is two levels removed from the root error
+    Caused by: Error: This error is one level removed from the root error
+    Caused by: Error: This error is the root error`)
   })
 
   it('pretty prints multiple suggestions', async () => {
@@ -74,6 +91,48 @@ describe('pretty-print', () => {
       const error = new Error('oh no!')
       error.stack = 'this is the error stack property'
       expect(prettyPrint(error)).to.equal('this is the error stack property')
+    })
+
+    it('shows the stack for causative errors', async () => {
+      const rootError: Error & PrettyPrintableError = new Error('This error is the root error')
+      rootError.stack = String.raw`Root error stack. In the wild, this would be something like 'Error: someMessage\n\tcodeStack'`
+      const errorOnceRemoved: Error & PrettyPrintableError = new Error(
+        'This error is one level removed from the root error',
+        {cause: rootError},
+      )
+      errorOnceRemoved.stack = String.raw`Once-removed error stack. In the wild, this would be something like 'Error: someMessage\n\tcodeStack'`
+      const errorTwiceRemoved: Error & PrettyPrintableError = new Error(
+        'This error is two levels removed from the root error',
+        {cause: errorOnceRemoved},
+      )
+      errorTwiceRemoved.stack = String.raw`Twice-removed error stack. In the wild, this would be something like 'Error: someMessage\n\tcodeStack'`
+
+      expect(prettyPrint(errorTwiceRemoved)).to.equal(
+        "Twice-removed error stack. In the wild, this would be something like 'Error: someMessage\\n\\tcodeStack'\n" +
+          "Caused by: Once-removed error stack. In the wild, this would be something like 'Error: someMessage\\n\\tcodeStack'\n" +
+          String.raw`Caused by: Root error stack. In the wild, this would be something like 'Error: someMessage\n\tcodeStack'`,
+      )
+    })
+
+    it('when no stack is available, standard pretty print is used instead', () => {
+      const rootError: Error & PrettyPrintableError = new Error('This error is the root error')
+      rootError.stack = String.raw`Root error stack. In the wild, this would be something like 'Error: someMessage\n\tcodeStack'`
+      const errorOnceRemoved: Error & PrettyPrintableError = new Error(
+        'This error is one level removed from the root error',
+        {cause: rootError},
+      )
+      delete errorOnceRemoved.stack
+      const errorTwiceRemoved: Error & PrettyPrintableError = new Error(
+        'This error is two levels removed from the root error',
+        {cause: errorOnceRemoved},
+      )
+      errorTwiceRemoved.stack = String.raw`Twice-removed error stack. In the wild, this would be something like 'Error: someMessage\n\tcodeStack'`
+
+      expect(prettyPrint(errorTwiceRemoved)).to.equal(
+        "Twice-removed error stack. In the wild, this would be something like 'Error: someMessage\\n\\tcodeStack'\n" +
+          'Caused by: Error: This error is one level removed from the root error\n' +
+          String.raw`Caused by: Root error stack. In the wild, this would be something like 'Error: someMessage\n\tcodeStack'`,
+      )
     })
   })
 })
